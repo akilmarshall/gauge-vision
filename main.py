5f40daeab3c20f3e70d95f341d14a7c7effa7470
Akil Marshall <akilmarshall@gmail.com>
2022-08-11 17:27:00 -1000

    removed erode

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import numpy as np
 import cv2 as cv
+
+
+red = (0,0,255)
+green = (0, 255, 0)
 img = cv.imread('example/2.png',0)
-img = cv.GaussianBlur(img, (5, 5), 0)
+img = cv.GaussianBlur(img, (7, 7), 0)
+img_edges = cv.Canny(img, 100, 200)
 cimg = cv.cvtColor(img,cv.COLOR_GRAY2BGR)
-circles = cv.HoughCircles(
-        img,
-        cv.HOUGH_GRADIENT,  # method
-        1,                  # dp
-        50,                 # minDist
-        param1=300, 
-        param2=150,
-        )
-circles = np.uint16(np.around(circles))
-print(circles)
-for i in circles[0,:]:
-    # draw the outer circle
-    cv.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-    cv.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+# circles = cv.HoughCircles(
+#         img,
+#         cv.HOUGH_GRADIENT,  # method
+#         1,                  # dp
+#         50,                 # minDist
+#         param1=300, 
+#         param2=150,
+#         )
+# circles = np.uint16(np.around(circles))
+# for [[x, y, r]] in circles:
+#     # draw the outer circle
+#     cv.circle(cimg,(x, y), r, green, 2)
+#     # draw the center of the circle
+#     cv.circle(cimg,(x, y) ,2, red,3)
+
+
+lines = cv.HoughLinesP(
+        img_edges,
+        1,                  # rho
+        np.pi / 180,    # theta
+        150,                 # threashold
+        None,               # min line len
+        0,                 # max line gap
+    )
+for [[a, b, c, d]] in lines:
+    cv.line(cimg, (a, b), (c, d), green, 2)
 cv.imwrite('out.png', cimg)
+cv.imwrite('edges.png', img_edges)
